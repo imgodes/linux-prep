47ac43e41a8b8f15e335f0b4b6a9fb5aeac8d4ae
Gabriel Guedes <52048434+chewygg@users.noreply.github.com>
2021-10-01 14:45:12 -0300

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 read -p "usuário: " USER
 read -p "senha: " PASSWORD
-read -p "home: " HOME
 read -p "pubkey: " PUBKEY
 getent passwd $USER > /dev/null 2&>1
 if [ $? -eq 0 ]; then
     echo "Usuário já criado"
 else
-  useradd -m -d /home/$USER -s /bin/bash $USER
+  	useradd -m -d /home/$USER -s /bin/bash $USER
 	usermod -aG sudo $USER
-	echo "$2" >> /home/$USER/.ssh/authorized_keys
+	mkdir -p /home/$USER/.ssh
+	echo "$PUBKEY" >> /home/$USER/.ssh/authorized_keys
 	chown -R $USER:$USER /home/$USER/.ssh
 	find /home -iname "*ssh" -exec chmod 700 {} \;
 	find /home -iname "authorized_keys*" -exec chmod 600 {} \;
