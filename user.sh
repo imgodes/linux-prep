89420f2b46825059ebe4eed9e0a60d368e08a53d
Gabriel Guedes <52048434+chewygg@users.noreply.github.com>
2021-10-01 14:31:50 -0300

    Update user.sh

--- user.sh
+++ user.sh
@@ -7,12 +7,12 @@ getent passwd $1 > /dev/null 2&>1
 if [ $? -eq 0 ]; then
     echo "Usuário já criado"
 else
-  useradd -m -d /home/$1 -s /bin/bash $1
-	usermod -aG sudo $1
-	echo "$2" >> /home/eferreira/.ssh/authorized_keys
-	chown -R $1:$1 /home/$1/.ssh
+  useradd -m -d /home/$USER -s /bin/bash $USER
+	usermod -aG sudo $USER
+	echo "$2" >> /home/$USER/.ssh/authorized_keys
+	chown -R $USER:$USER /home/$USER/.ssh
 	find /home -iname "*ssh" -exec chmod 700 {} \;
 	find /home -iname "authorized_keys*" -exec chmod 600 {} \;
-	echo '$1:$3' | chpasswd
-	chage -d 0 $1
+	echo '$USER:$PASSWORD' | chpasswd
+	chage -d 0 $USER
 fi
